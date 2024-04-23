99a302cac02168c91486afa8ce2ab37cca2c3721
Jonathan Stahl <jonathanjstahl7@gmail.com>
2024-04-23 17:41:35 +1000

    more events stuff

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 
-// When the user clicks on <div>, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
 
 /* 
 let slideIndex = 1;
